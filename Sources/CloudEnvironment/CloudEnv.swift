64921dd9023c5861e4c5daaa049e42280d59f058
Aaron Liberatore <aaron.m.liberatore@ibm.com@aarons-mbp.austin.ibm.com>
2017-09-29 10:37:40 -0500

    Swift 4.0 support

--- Sources/CloudEnvironment/CloudEnv.swift
+++ Sources/CloudEnvironment/CloudEnv.swift
@@ -170,7 +170,11 @@ public class CloudEnv {
       guard let range = path.range(of: "/") else {
         return path
       }
-      return path.substring(from: range.upperBound)
+      #if swift(>=3.2)
+        return String(path[range.upperBound...])
+      #else
+        return path.substring(from: range.upperBound)
+      #endif
     }
     return path
   }
